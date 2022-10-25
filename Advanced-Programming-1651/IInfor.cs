54c7a1e9e38f59739fc4662e2673df83cb140b40
thanhvo1234 <thanhvnkbgcd201808@fpt.edu.vn>
2022-10-26 05:59:39 +0700

    fix error don't up code

--- Advanced-Programming-1651/IInfor.cs
+++ Advanced-Programming-1651/IInfor.cs
@@ -1,4 +1,13 @@
-﻿namespace Advanced_Programming_1651
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Advanced_Programming_1651;
+
+
+
+namespace Advanced_Programming_1651
 {
     public interface IInfor
     {
--- Advanced-Programming-1651/SuperCar.cs
+++ Advanced-Programming-1651/SuperCar.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Advanced_Programming_1651;
 
 namespace Advanced_Programming_1651
 {
-    internal class SuperCar
+    internal class SuperCar : Car
     {
+        public SuperCar(int id, string brand, string _model, double price) : base(id, brand, _model, price)
+        {
+        }
+
+        public override double Price
+        {
+            get
+            {
+                return base.Price * 1.3;
+            }
+            set
+            {
+                base.Price = value;
+            }
+        }
     }
 }
